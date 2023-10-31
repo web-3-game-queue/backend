a142e8a67daee20e63be0afdfd85250ac8d57705
alordash <alordash@mail.ru>
2023-10-31 15:02:41 +0300

    Adding core project

---
+++ GameQueue/GameQueue.Core/Models/Users/User.cs
@@ -0,0 +1,12 @@
+﻿namespace GameQueue.Core.Models.Users;
+
+public sealed record class User
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string HashedPassword { get; set; }
+
+    public int Level { get; set; }
+}
